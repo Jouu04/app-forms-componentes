575465c2cb5094187b5d59751f3ac84bc74e4e6b
Jouu04 <jhoulozanohuaymacari@gmail.com>
2024-06-28 22:54:32 -0500

    3

--- controller/MayorMenorController.py
+++ controller/MayorMenorController.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic
+from service import MayorMenorService
 
 class MayorMenor:
 
@@ -6,5 +7,25 @@ class MayorMenor:
         app = QtWidgets.QApplication([])
 
         self.ventana = uic.loadUi("view/frmmayormenor.ui")
+        self.ventana.lbcalcular.clicked.connect(self.onclickbtncalcular)
         self.ventana.show()
         app.exec()
+    
+    def onclickbtncalcular(self):
+        resultado = 0
+        try:
+            n1 = int(self.ventana.txtnum1.txt())
+            n2 = int(self.ventana.txtnum2.txt())
+            n3 = int(self.ventana.txtnum3.txt())
+            n4 = int(self.ventana.txtnum4.txt())
+            if self.ventana.rbmayor.isChecked():
+                resultado = MayorMenorService.mayor(n1,n2,n3,n4)
+            elif self.ventana.rbmayor.isChecked():
+                resultado = MayorMenorService.menor(n1,n2,n3,n4)
+            else:
+                resultado = 0
+        except:
+            resultado = 0
+
+        finally:
+            self.ventana.asd.setText(str(resultado))
--- main..py
+++ main..py
@@ -3,8 +3,8 @@ from controller.ComboCascada import ComboCascada
 from controller.ComboSimple import ComboSimple
 from controller.MayorMenorController import MayorMenor
 
-formulario1 = CalculadoraController()
+#formulario1 = CalculadoraController()
 #formulario2 = ComboCascada()
 #formulario3 = ComboSimple()
-#formulario4 = MayorMenor()
+formulario4 = MayorMenor()
 
